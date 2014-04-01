341bba97ce67cd2e2d7bbe57163049e73b9e8d81
gabriel <gabriel@lc41.com>
2014-04-01 00:32:08 -0700

    Playing with scope support

--- lib/datum/scenario_helper.rb
+++ lib/datum/scenario_helper.rb
@@ -42,8 +42,8 @@ module ScenarioHelper
 
       unless ref_scope.nil?
         ref_scope = self.send(ref_scope)
-        Thread.current[:account_id] = ref_scope
-        raise "#{ref_scope}"
+        Thread.current[:account_id] = ref_scope.id
+        raise "#{ref_scope.id}"
       end
 
       if 0 != ref_hash.count
