46cf92f6e895345e260f08c2c62c69cb1cb88d7e
gabriel <gabriel@lc41.com>
2014-12-05 04:32:39 -0800

    Almost working, but ugly again.

--- lib/datum.rb
+++ lib/datum.rb
@@ -7,24 +7,53 @@
 require 'datum/railtie'
 require "datum/structures"
 require "scenario"
-
+#DatumInfo = Struct.new(:tst, :name, :list)
 module Datum
+  @@managed_data = {}; @@directory = nil
 
   def self.directory
-    Rails.root.join('test', 'datum', 'data').to_s + File::Separator
+    @@directory ||= Rails.root.join(
+      'test', 'datum', 'data').to_s + File::Separator
   end
 
-  def self.read file
-    File.read("#{directory}#{file}.rb")
+  def self.read(file); File.read("#{directory}#{file}.rb"); end
+  def self.read_datum(key); @@managed_data[key]; end
+  def self.add_datum(key, val); @@managed_data[key] = val; end
+
+  class DataManager
+
+    def initialize(tst, file)
+      @testcase = tst; @filename = file; @counter = 0
+      self.class.constants.each {|c| self.class.send(:remove_const, c)}
+    end
+    def self.test_name(file, counter);"test_#{file}_#{counter}";end
+    def self.datum_key(testcase, test_name); "#{testcase}##{test_name}"; end
+
+
+    def process(&block)
+      @testcase.send(:define_method, @filename, &block)
+      eval Datum.read(@filename)
+    end
+
+    def add_structure struct
+      @counter += 1; nm = DataManager.test_name(@filename, @counter)
+      k = DataManager.datum_key(@testcase, nm);
+      Datum.add_datum k, struct
+      @testcase.class_eval do
+        define_method nm do
+          full_key = DataManager.datum_key(self.class.to_s, __method__)
+          @datum = Datum.read_datum(full_key)
+          self.send @datum.data_method
+        end
+      end
+      return @filename
+    end
+
   end
-
-
-  DatumInfo = Struct.new(:tst, :name, :list)
 
 end
 
-
-
-def data_test data_filename, &block
-  #Datum.process data_filename, &block, klass
+def data_test file, &block
+  $datum_data_manager = Datum::DataManager.new(self, file)
+  $datum_data_manager.process(&block)
 end
--- lib/datum/structures.rb
+++ lib/datum/structures.rb
@@ -2,6 +2,10 @@ require "plan9/structures"
 
 #module Datum
 class DatumStruct < Plan9::ImprovedStruct
+  def self.new(*attrs, &block)
+    attrs.push(:data_method)
+    super(*attrs, &block)
+  end
 
 protected
   def self.init_new(struct)
@@ -16,6 +20,7 @@ private
     alias_method :improved_initialize, :initialize
 
     def initialize(*attrs)
+      attrs.push($datum_data_manager.add_structure(self))
       improved_initialize(*attrs)
     end
   end
