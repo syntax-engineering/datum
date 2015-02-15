470b8e7581ba56c3eb75fb2636cb9f283b1f4c05
gabriel <gabriel@lc41.com>
2015-02-15 01:15:18 -0800

    Playing with Yard (still)

--- lib/support/scenario.rb
+++ lib/support/scenario.rb
@@ -2,26 +2,26 @@
 # For use in a scenario file, imports an existing scenario's code and objects
 #
 # @example Import an Existing Scenario
-#   ## file test/datum/scenarios/original_scenario.rb
-#   @clancy = Person.create(
+#    # file test/datum/scenarios/original_scenario.rb
+#    @clancy = Person.create(
 #     first_name: "Clancy",
 #     last_name: "Wiggum")
 #
-#   ## file test/datum/scenarios/example_scenario.rb
-#   @marge = Person.create(
+#    # file test/datum/scenarios/example_scenario.rb
+#    @marge = Person.create(
 #     first_name: "Marge",
 #     last_name: "Simpson")
 #
-#   __import :original_scenario # will give this scenario access to @clancy
+#    __import :original_scenario # will give this scenario access to @clancy
 #
-#   ## file test/models/person_test.rb
-#   test "check name" do
+#    # file test/models/person_test.rb
+#    test "check name" do
 #     process_scenario :example_scenario
 #     assert_not_nil @marge.first_name # from example_scenario
 #     assert_not_nil @clancy.first_name # from original_scenario (imported)
-#   end
+#    end
 #
-# @param [:symbol, String] scenario_name The name of a scenario file
+# @param [symbol, String] scenario_name The name of a scenario file
 def __import scenario_name
   ::Datum::Helpers.import_file scenario_name, ::Datum.scenario_path, binding
 end
@@ -29,19 +29,19 @@ end
 # For use in a scenario file, clones the attributes of an existing instance
 #
 # @example Cloning an Existing Resource
-#   ## file test/datum/scenarios/clone_example_scenario.rb
-#   @marge = Person.create(
+#    # file test/datum/scenarios/clone_example_scenario.rb
+#    @marge = Person.create(
 #     first_name: "Marge",
 #     last_name: "Simpson")
 #
-#   @homer = Person.create(__clone(@marge, {first_name: "Homer"}))
+#    @homer = Person.create(__clone(@marge, {first_name: "Homer"}))
 #
-#   ## file test/models/another_person_test.rb
-#   test "check name again" do
+#    # file test/models/another_person_test.rb
+#    test "check name again" do
 #     process_scenario :clone_example_scenario
 #     assert_equal "Marge", @marge.first_name
 #     assert_equal "Homer", @homer.first_name
-#   end
+#    end
 #
 # @param [ActiveRecord::Base] resource An ActiveRecord Model instance
 # @param [Hash] override_hash Hash of attributes / values to override from
