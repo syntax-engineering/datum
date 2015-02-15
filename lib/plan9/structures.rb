d5d77fcc76137d5ed0648ca23c46d20667285972
gabriel <gabriel@lc41.com>
2015-02-14 23:11:50 -0800

    Trying to see if @private works.

--- lib/plan9/structures.rb
+++ lib/plan9/structures.rb
@@ -1,28 +1,33 @@
 # @private
-# Module for simple code reuse between various projects.
-module Plan9
+module Plan9 # Module for simple code reuse between various projects.
 
-# A few improvements to a Ruby Struct
+# @private
+class ImprovedStruct # A few improvements to a Ruby Struct
 #
 # Re-organized slightly, this code is reused from 'ImmutableStruct'
 # by Theo Hultberg. See https://github.com/iconara/immutable_struct
 # Copyright notice mentioned in the LICENSE file.
 #
-class ImprovedStruct
+  # @private
   def self.new(*attrs, &block)
     init_new(Struct.new(*attrs, &block))
   end
+
 protected
+  # @private
   def self.init_new(struct)
     optionalize_constructor!(struct)
     extend_dup!(struct)
     struct
   end
+
 private
+  # @private
   def self.optionalize_constructor!(struct)
     struct.class_eval do
       alias_method :struct_initialize, :initialize
 
+      # @private
       def initialize(*attrs)
         if is_hash_case?(*attrs)
           struct_initialize(*members.map { |m| attrs.first[m.to_sym] })
@@ -32,13 +37,14 @@ private
       end
 
     protected
-      # @return (bool) true if attrs are Hash, false otherwise
+      # @private
       def is_hash_case?(*a)
+      # @return (bool) true if attrs are Hash, false otherwise
         members.size > 1 && a && a.size == 1 && a.first.instance_of?(Hash)
       end
     end
   end
-
+  # @private
   def self.extend_dup!(struct)
     struct.class_eval do
       def dup(overrides={})
@@ -48,19 +54,22 @@ private
   end
 end
 
-# A read-only Struct
-#
+
+# @private
+class ImmutableStruct < ImprovedStruct # A read-only Struct
 # Re-organized slightly, this code is reused from 'ImmutableStruct'
 # by Theo Hultberg. See https://github.com/iconara/immutable_struct
 # Copyright notice mentioned in the LICENSE file.
-class ImmutableStruct < ImprovedStruct
+
 protected
+  # @private
   def self.init_new(struct)
     make_immutable!(struct)
     super(struct)
   end
 
 private
+  # @private
   def self.make_immutable!(struct)
     # removes the member= method, to prevent write
     struct.send(:undef_method, "[]=".to_sym)
