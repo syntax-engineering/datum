bf421bdf7b5c962f49ac7df8c5ce2f84db230313
gabriel <gabriel@lc41.com>
2015-02-15 01:58:48 -0800

    Playing with Yard (still)

--- lib/support/scenario.rb
+++ lib/support/scenario.rb
@@ -1,16 +1,13 @@
 
 # For use in a scenario file, imports an existing scenario's code and objects
-#
 # @param [symbol, String] scenario_name The name of a scenario file
 def __import scenario_name
   ::Datum::Helpers.import_file scenario_name, ::Datum.scenario_path, binding
 end
 
 # For use in a scenario file, clones the attributes of an existing instance
-#
 # @param [ActiveRecord::Base] resource An ActiveRecord Model instance
 # @param [Hash] override_hash Hash of attributes / values to override from
-#
 # @return [Hash] Hash of attributes from provided resource
 def __clone resource, override_hash = nil
   ::Datum::Helpers.clone_resource resource, override_hash
--- lib/support/test.rb
+++ lib/support/test.rb
@@ -4,12 +4,10 @@ require "datum/datum"
 
 # Extends ActiveSupport::Test with the process_scenario method and includes
 # the Datum module
-#
 # @note Supports most extending test types (functional, integration, etc)
 class ActiveSupport::TestCase
   include Datum
   # Imports a scenario file into the execution context of the current test
-  #
   # @param [:symbol, String] scenario_name The name of a scenario file
   def process_scenario scenario_name
     __import(scenario_name)
@@ -17,8 +15,7 @@ class ActiveSupport::TestCase
 end
 
 # Defines a test to work in conjuction with Datum struct extensions found in
-# a file with the same name in the test/datum/data directory
-#
+# a file with the same name in the test/datum/data directory.
 # @param [String] name Name of the file in the datum/data directory
 # @param [Block] block A block of Ruby code
 def data_test name, &block
