63d8daa2add8f9272635eb30c288effcd88b7024
gabriel <gabriel@lc41.com>
2015-02-15 01:06:52 -0800

    Playing with Yard (still)

--- lib/support/scenario.rb
+++ lib/support/scenario.rb
@@ -27,6 +27,22 @@ def __import scenario_name
 end
 
 # For use in a scenario file, clones the attributes of an existing instance
+#
+# @example Cloning an Existing Resource
+#   ## file test/datum/scenarios/clone_example_scenario.rb
+#   @marge = Person.create(
+#     first_name: "Marge",
+#     last_name: "Simpson")
+#
+#   @homer = Person.create(__clone(@marge, {first_name: "Homer"}))
+#
+#   ## file test/models/another_person_test.rb
+#   test "check name again" do
+#     process_scenario :clone_example_scenario
+#     assert_equal "Marge", @marge.first_name
+#     assert_equal "Homer", @homer.first_name
+#   end
+#
 # @param [ActiveRecord::Base] resource An ActiveRecord Model instance
 # @param [Hash] override_hash Hash of attributes / values to override from
 # @return [Hash] Hash of attributes from provided resource
