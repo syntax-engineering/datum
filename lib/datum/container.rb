21b10c987af444635064e6285efa126fe052af4c
gabriel <gabriel@lc41.com>
2015-02-15 02:45:15 -0800

    Still... working on Yard.

--- lib/datum/container.rb
+++ lib/datum/container.rb
@@ -5,7 +5,6 @@ module Datum
 class Container
 
   # Creates a Hash key formatted for use with a Container.
-  #
   # @param [TestCase] tst_instance The TestCase instance for the data_test
   # @param [String] data_method_name The name of the data_test method
   # @return [String] Container compatible Hash key
