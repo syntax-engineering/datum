1ef13416730b867037942610b0fad44f21f7168e
gabriel <gabriel@lc41.com>
2015-02-16 02:47:19 -0800

    doc updates

--- lib/datum.rb
+++ lib/datum.rb
@@ -5,7 +5,29 @@ require "datum/container"
 require "support/test"
 require "support/scenario"
 
-# Datum is a flexible data-driven test solution for Rails
+# Datum is a flexible data-driven test solution for Rails. Datum's primary
+# features include:
+#
+# 1) The method data_test, for making test cases which are generated from
+#    datasets you define.
+# 2) Scenarios, which are a load-on-demand mechanism for seeding the test db.
+#
+# The Datum Parts & API:
+#   + Datum.[Class Attributes] are calls to Datum and Scenario paths as
+#     well as access to all Containers.
+#
+#   + Top Level Namespace provides access to data_test (for use in your
+#     tests directly) and a couple of basic Scenario helpers
+#     (__import, __clone, etc).
+#
+#   + ActiveSupport::TestCase is modified to include Datum and add the
+#     process_scenario method.
+#
+#   + Datum::ImmutableStruct is a struct for defining sub-classed datasets to
+#     use with tests defined via data_test.
+#
+#   + Container is a high-level storage of each data_test and it's associated
+#     datasets, test instance, etc
 module Datum
   @@all_containers, @@scenario_path, @@data_path, @@datum_path = nil
 
