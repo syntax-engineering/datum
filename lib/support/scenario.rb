1e296d176ff01ce3be238303e83c105d2abd986f
gabriel <gabriel@lc41.com>
2015-02-16 01:26:06 -0800

    doc updates

--- lib/support/scenario.rb
+++ lib/support/scenario.rb
@@ -6,8 +6,13 @@ def __import scenario_name
   ::Datum::Helpers.import_file scenario_name, ::Datum.scenario_path, binding
 end
 
-# For use in a scenario file, clones the attributes of an existing instance
-#
+# From a scenario file, clones the attributes of an existing instance and
+# overrides as specified.
+# @example Using __clone
+#   # test/datum/scenarios/simpsons_scenario.rb
+#   # any code included in this file gets loaded when calling process_scenario
+#   @homer = Person.create(first_name: "Homer", last_name: "Simpson")
+#   @marge = Person.create(__clone(@homer, {first_name: "Marge"}))
 # @param [ActiveRecord::Base] resource An ActiveRecord Model instance
 # @param [Hash] override_hash Hash of attributes / values to override from
 # @return [Hash] Hash of attributes from provided resource
