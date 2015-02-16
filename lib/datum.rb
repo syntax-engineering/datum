b3d055d4b02ed0704f17ef65f44f220e33ce321b
gabriel <gabriel@lc41.com>
2015-02-16 02:57:17 -0800

    doc updates

--- lib/datum.rb
+++ lib/datum.rb
@@ -10,20 +10,21 @@ require "support/scenario"
 # generated from datasets you define. 2) Scenarios, a load-on-demand
 # mechanism for seeding the test db.
 #
-# The Datum Parts & API:
-# + Datum.[Class Attributes] calls to Datum and Scenario paths as
-# well as access to all Containers.
+# Datum includes:
 #
-# + Top Level Namespace provides access to data_test (for use in your tests
+# Datum.[Class Attributes] calls to paths as well as access to
+# all Containers.
+#
+# Top Level Namespace provides access to data_test (for use in your tests
 # directly) and a couple of basic Scenario helpers (__import, __clone, etc).
 #
-# + ActiveSupport::TestCase is modified to include Datum and add the
+# ActiveSupport::TestCase is modified to include Datum and add the
 # process_scenario method.
 #
-# + Datum::ImmutableStruct is a struct for defining sub-classed datasets to
+# Datum::ImmutableStruct is a struct for defining sub-classed datasets to
 # use with tests defined via data_test.
 #
-# + Container is a high-level storage of each data_test and it's associated
+# Container is a high-level storage of each data_test and it's associated
 # datasets, test instance, etc
 module Datum
   @@all_containers, @@scenario_path, @@data_path, @@datum_path = nil
